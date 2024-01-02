95d22f61e4f8b7d6033cf48f38fd9c86436870d4
Ashok734 <meashok850@gmail.com>
2024-01-02 13:54:37 +0545

    custom model login

---
+++ product/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Product
+# Create your views here.
+def product(request):
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'index.html', context)
